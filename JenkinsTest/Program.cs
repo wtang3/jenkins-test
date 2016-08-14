55f882955c88d83eaae6cece87d7e8a71483a7d1
wtang <wtang3@outlook.com>
2016-08-14 03:52:38 -0400

    initial commit

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTest
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+
+        public string HelloJenkins()
+        {
+            return "Hello Jenkins";
+        }
+
+        public int Add(int num, int num2)
+        {
+            return num + num2;
+        }
+    }
+}
---
+++ JenkinsUnitTest/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using JenkinsTest;
+using Xunit;
+
+namespace JenkinsUnitTest
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var a = new Program();
+
+            Assert.Matches("Hello Jenkins", a.HelloJenkins());
+        }
+
+        [Fact]
+        public void PassingTest2()
+        {
+            var a = new Program();
+
+            Assert.Matches("Hello Jenkins", a.HelloJenkins());
+        }
+
+        [Fact]
+        public void PassingTest3()
+        {
+            var a = new Program();
+
+            Assert.Equal(2,a.Add(1,1));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            var a = new Program();
+
+            Assert.Matches("Hello Jenkinss", a.HelloJenkins());
+        }
+
+    }
+}
